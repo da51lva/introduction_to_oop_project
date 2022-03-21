754bf9c1f4e4b152d61571717000765526f45028
Matt DaSilva <matt_das@hotmail.co.uk>
2022-03-21 16:09:15 +0000

    Complete Activity 3

--- src/main/java/com/techreturners/introduction_to_oop_project/ConsoleApplication.java
+++ src/main/java/com/techreturners/introduction_to_oop_project/ConsoleApplication.java
@@ -3,8 +3,15 @@ package com.techreturners.introduction_to_oop_project;
 public class ConsoleApplication {
 
     public static void main(String[] args){
+
         Bird myBird = new Bird("Tweety", "Yellow");
         myBird.speak();
         myBird.fly();
+
+        Pigeon pippa = new Pigeon("Pippa", "Grey");
+        pippa.speak();
+        pippa.fly();
+        pippa.eatPizza();
+        pippa.sleep();
     }
 }
---
+++ src/main/java/com/techreturners/introduction_to_oop_project/Pigeon.java
@@ -0,0 +1,19 @@
+package com.techreturners.introduction_to_oop_project;
+
+//The Pigeon extends the base class Bird
+public class Pigeon extends Bird {
+
+    //The Pigeon constructor calls the base class Bird constructor
+    //The name and color parameters are passed to the Bird constructor
+    public Pigeon(String name, String color) {
+        super(name, color);
+    }
+
+    public void eatPizza() {
+        System.out.println("Delicious pizza!");
+    }
+
+    public void sleep(){
+        System.out.println("I'm a sleeping pigeon");
+    }
+}
